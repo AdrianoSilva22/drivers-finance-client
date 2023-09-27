65f74117435e565274909f81e0e8c5de2ded4df6
AdrianoSilva22 <adrianodesilva22@gmail.com>
2023-09-27 13:50:27 -0300

    front em processo

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -3,6 +3,7 @@
 import { mensagemErro, mensagemSucesso } from "@/models/toastr"
 import { Usuario, valorInicialUsuario } from "@/models/usuario"
 import { cadastroService } from "@/service/cadastroService"
+import '@/styles/cadastro.css'
 import { useState } from "react"
 
 interface propriedadeErros {
@@ -35,49 +36,61 @@ export default function Cadastro() {
     }
 
     return (
-        <div>
-            <label>Login:</label>
-            <input
-                type="text"
-                name="login"
-                onChange={(e) => setUsuario({ ...usuario, login: e.target.value })}
-            />
+        <div className="Principal">
+            <div className="secundaria">
+                <div className="detalhes">
+                    <h1>Cadastro de Usuários</h1>
+                    <div className="linha-horizontal"></div>
+                </div>
+                <div className="terciaria">
+                    <label>Login:</label>
+                    <input
+                        type="text"
+                        name="login"
+                        onChange={(e) => setUsuario({ ...usuario, login: e.target.value })}
+                    />
 
-            <label>Nome:</label>
-            <input
-                type="text"
-                name="nome"
-                onChange={(e) => setUsuario({ ...usuario, nome: e.target.value })}
-            />
+                    <label>Nome:</label>
+                    <input
+                        style="background-image: url('caminho-para-sua-imagem.jpg')" 
+                        placeholder="Nome"
+                        type="text"
+                        name="nome"
+                        onChange={(e) => setUsuario({ ...usuario, nome: e.target.value })}
+                    />
 
-            <label>Email:</label>
-            <input
-                type="email"
-                name="email"
-                onChange={(e) => setUsuario({ ...usuario, email: e.target.value })}
-            />
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        name="email"
+                        onChange={(e) => setUsuario({ ...usuario, email: e.target.value })}
+                    />
 
-            <label>Senha:</label>
-            <input
-                type="password"
-                name="senha"
-                onChange={(e) => setUsuario({ ...usuario, senha: e.target.value })}
-            />
+                    <label>Senha:</label>
+                    <input
+                        type="password"
+                        name="senha"
+                        onChange={(e) => setUsuario({ ...usuario, senha: e.target.value })}
+                    />
 
-            <label>Gênero:</label>
-            <select
-                name="genero"
-                id="genero"
-                onChange={(e) => setUsuario({ ...usuario, genero: e.target.value })} >
-                <option value="" >Selecione...</option>
-                <option value="feminino">Femino</option>
-                <option value="masculino">Masculino</option>
-                <option value="outro">Outro</option>
-            </select>
-
-            <button
-                type="button"
-                onClick={cadastrar}>Cadastrar</button>
+                    <label>Gênero:</label>
+                    <select
+                        name="genero"
+                        id="genero"
+                        onChange={(e) => setUsuario({ ...usuario, genero: e.target.value })} >
+                        <option value="" >Selecione...</option>
+                        <option value="feminino">Femino</option>
+                        <option value="masculino">Masculino</option>
+                        <option value="outro">Outro</option>
+                    </select>
+                    <div className="div-Buttao">
+                    <button
+                        type="button"
+                        onClick={cadastrar}>Cadastrar
+                    </button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { mensagemErro, mensagemSucesso } from "@/models/toastr"
 import { Usuario, valorInicialUsuario } from "@/models/usuario"
 import { loginService } from "@/service/loginService"
 import { useState } from "react"
@@ -6,21 +7,22 @@ const { login } = loginService()
 export default function Login() {
 
     const [usuario, setUsuario] = useState<Usuario>(valorInicialUsuario)
+    console.log(usuario)
 
     const logar = async () => {
         try {
             const response = await login(usuario)
-            console.log(response)
-        } catch (e) {
-            console.log(e)
+            mensagemSucesso(response.data)
+        } catch (e: any) {
+            mensagemErro(e.response.data)
         }
     }
     return (
         <div>
             <label>Login</label>
-            <input type="text" name="Login" id="Login" onChange={(e) => setUsuario({ ...usuario, login: e.target.value })} />
+            <input type="text" name="Login" id="Login" onChange={(e) => setUsuario({...usuario, login: e.target.value })} />
             <label>Senha</label>
-            <input type="password" name="senha" id="senha" onChange={(e) => setUsuario({ ...usuario, senha: e.target.value })} />
+            <input type="password" name="senha" id="senha" onChange={(e)=>{setUsuario({...usuario,senha:e.target.value})}}/>
             <button type="button" onClick={logar}>Login</button>
 
         </div>
