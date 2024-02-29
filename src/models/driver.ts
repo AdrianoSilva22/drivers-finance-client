0bb68183cdd6e1e573cd81e8fbc806b3167306cc
Adriano da Silva <126006598+AdrianoSilva22@users.noreply.github.com>
2024-02-29 19:02:08 -0300

    criando models de expense, driver e income

---
+++ src/models/driver.ts
@@ -0,0 +1,17 @@
+export interface Driver {
+  cpf: string
+  name: string
+  email: string
+  senha: string
+  genero: string
+  phone_number: string
+}
+
+export const valorInicialDriver: Driver = {
+  cpf: '',
+  name: '',
+  email: '',
+  senha: '',
+  genero: '',
+  phone_number: ''
+}
