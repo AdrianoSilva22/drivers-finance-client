e0bf09c02eae33b4599a06682285990502878420
Adriano da Silva <126006598+AdrianoSilva22@users.noreply.github.com>
2024-03-30 00:49:18 -0300

    criando Home de teste para mostrar usuários

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,38 @@
+'use client'
+
+import { DriverCard } from "@/components/UsuarioCard";
+import { Driver } from "@/models/driver";
+import { driverService } from "@/service/driverService";
+import '@/styles/usuarioCard.css';
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
+  const { getTotalDriver } = driverService()
+  const [drivers, setDrivers] = useState<Driver[]>([])
+
+  useEffect(() => {
+    const loadDrivers = async () => {
+      try {
+        const response = await getTotalDriver()
+        setDrivers(response.data)
+      } catch (error) {
+        console.log(error + "deu ruim ");
+
+      }
+    }
+    loadDrivers()
+  })
+
+  const print = () => { console.log(drivers) }
+
   return (
     <div>
-      Aqui será a tela Home.
+      {
+        drivers.map(driver => {
+          return <DriverCard key={Number(driver.cpf)} driver={driver} />
+        })
+      }
     </div>
   )
 }
