9e31740db067e93885cdd12d0f97fe5a401b0dc4
Adriano da Silva <126006598+AdrianoSilva22@users.noreply.github.com>
2024-03-30 01:24:55 -0300

    criado um middleware para assegurar as rotas protegidas, verifica se o usuário está realmente logado e possui token.

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+    
+export default function ola(request: NextRequest) {
+    console.log('chegou')
+    const token = request.cookies.get('driverToken')?.value
+    const signInURL = new URL('/driver/login', request.url)
+    if (!token) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.next()
+        }
+        return NextResponse.redirect(signInURL)
+    }
+}
+
+export const config = {
+    matcher: ['/dashboard/:path*']
+}
