78613f15be1442f5fd4bcb195c2d1bf36d5177c5
Adriano da Silva <126006598+AdrianoSilva22@users.noreply.github.com>
2024-03-30 00:39:45 -0300

    alterando css e mudando title do metadada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Lista de Usuários',
+  title: 'Drivers Finance',
   description: '',
 }
 
